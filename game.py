c19e32538ee8e40fd6bd88a5c3093dd67140aa84
CoolCream <mahasankarshant@gmail.com>
2025-08-15 20:24:20 +0530

    Made code

--- game.py
+++ game.py
@@ -1,18 +1,50 @@
 import pygame,sys
 
-pygame.init()
+from scripts.entities import PhysicsEntity
+from scripts.utils import load_img
 
+class Game():
+    def __init__(self):
+        pygame.init()
 
+        pygame.display.set_caption("Ninja Platformer")
+        self.screen = pygame.display.set_mode((640,480))
 
-screen = pygame.display.set_mode((640,480))
+        self.clock = pygame.time.Clock()
 
-clock = pygame.time.Clock()
+        self.movement = [False, False]
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        self.assets = {
+            'player': load_img('entities/player.png')
+        }
 
-    pygame.display.update()
-    clock.tick(60)
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+
+    def run(self):
+        while True:
+            self.screen.fill((14, 219, 248))
+
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.screen)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit() 
+                    sys.exit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+                        
+
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
